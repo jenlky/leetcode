0c7504eaefd488dc88c1a0b993df6863f37c3693
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2020-01-10 09:40:57 +0800

    Add function for repeated condition in FizzBuzz

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -10,13 +10,16 @@
  * @return {string[]}
  */
 const fizzBuzz = function(n) {
+  const divisibleBy3 = num => num % 3 === 0;
+  const divisibleBy5 = num => num % 5 === 0;
+
   const output = [];
   for (let x = 1; x <= n; x++) {
-    if (x % 15 === 0) {
+    if (divisibleBy3 && divisibleBy5) {
       output.push("FizzBuzz");
-    } else if (x % 5 === 0) {
+    } else if (divisibleBy5) {
       output.push("Buzz");
-    } else if (x % 3 === 0) {
+    } else if (divisibleBy3) {
       output.push("Fizz");
     } else {
       output.push(x.toString(10));
