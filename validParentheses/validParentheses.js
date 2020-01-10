95e88e453588b56beef6ad8339eb3d2da2ab0809
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2020-01-10 09:16:35 +0800

    Store parentheses as key-value pair

--- validParentheses/validParentheses.js
+++ validParentheses/validParentheses.js
@@ -14,35 +14,22 @@ const isValid = function(s) {
   if (odd) return false;
 
   const arrayChar = s.split("");
-  let pointer = 0;
-  const map = [];
+  const endings = [];
+  const map = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
+  };
 
   for (let char of arrayChar) {
-    switch (char) {
-      case "(":
-      case "{":
-      case "[":
-        map[pointer++] = char;
-        break;
-      case ")":
-        if (pointer === 0 || map[--pointer] !== "(") {
-          return false;
-        }
-        break;
-      case "}":
-        if (pointer === 0 || map[--pointer] !== "{") {
-          return false;
-        }
-        break;
-      case "]":
-        if (pointer === 0 || map[--pointer] !== "[") {
-          return false;
-        }
-        break;
+    if (char in map) {
+      endings.push(map[char]);
+    } else if (endings.length === 0 || endings.pop() !== char) {
+      return false;
     }
   }
 
-  return pointer === 0;
+  return endings.length === 0;
 };
 // @lc code=end
 module.exports = { isValid };
